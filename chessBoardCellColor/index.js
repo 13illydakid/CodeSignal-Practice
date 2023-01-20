a9742a7ee7b0f5ce2a6faa1824f55cc5ee023b07
Billy Chiu <billychiu@MacBook-AirM1sg.local>
2023-01-20 09:03:55 -0800

    chessBoardCellColor

---
+++ chessBoardCellColor/index.js
@@ -0,0 +1,15 @@
+function solution(cell1, cell2) {
+    let board = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 6,
+        "G": 7,
+        "H": 8
+    }
+    let total1 = board[cell1[0]] + parseInt(cell1[1]);
+    let total2 = board[cell2[0]] + parseInt(cell2[1]);
+    return total1 % 2 === total2 % 2;
+}
