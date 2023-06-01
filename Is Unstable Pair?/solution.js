3198c73b8a266cbfb4e3be751e23f10882d51823
Billy Chiu <billychiu517@gmail.com>
2023-05-31 23:06:04 -0700

    11_04_pm

--- Is Unstable Pair?/solution.js
+++ Is Unstable Pair?/solution.js
@@ -5,11 +5,20 @@ function solution(filename1, filename2) {
     if(filename2.length > filename1.length){
         longest = filename2.length;
     }
+    let array = [];
     for(let i = 0; i < longest; i++){
         let a = filename1[i];
         let b = filename2[i];
         let a1 = order.indexOf(a);
         let b1 = order.indexOf(b);
-
+        if(a1 !== b1){
+            if(a1 < b1){
+                array.push(a1);
+                array.push(b1);
+            } else {
+                array.push(b1);
+                array.push(a1);
+            }
+        }
     }
 }
