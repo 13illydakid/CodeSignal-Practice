3cf6df83957678c5262053ec08cd3f3185bab2ee
Billy Chiu <billychiu517@gmail.com>
2023-06-06 23:34:18 -0700

    11_32_pm

--- Is Unstable Pair?/solution.js
+++ Is Unstable Pair?/solution.js
@@ -1,24 +1,44 @@
+// function solution(filename1, filename2) {
+//     let order = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+//     //console.log(order.length);
+//     let longest = filename1.length;
+//     if(filename2.length > filename1.length){
+//         longest = filename2.length;
+//     }
+//     let array = [];
+//     for(let i = 0; i < longest; i++){
+//         let a = filename1[i];
+//         let b = filename2[i];
+//         let a1 = order.indexOf(a);
+//         let b1 = order.indexOf(b);
+//         if(a1 !== b1){
+//             if(a1 < b1){
+//                 array.push(a1);
+//                 array.push(b1);
+//             } else {
+//                 array.push(b1);
+//                 array.push(a1);
+//             }
+//         }
+//     }
+// }
+
 function solution(filename1, filename2) {
-    let order = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    //console.log(order.length);
-    let longest = filename1.length;
-    if(filename2.length > filename1.length){
-        longest = filename2.length;
+    let primaryOrder;
+    if(filename1 < filename2){
+        primaryOrder = true;
+    } else{
+        primaryOrder = false;
     }
-    let array = [];
-    for(let i = 0; i < longest; i++){
-        let a = filename1[i];
-        let b = filename2[i];
-        let a1 = order.indexOf(a);
-        let b1 = order.indexOf(b);
-        if(a1 !== b1){
-            if(a1 < b1){
-                array.push(a1);
-                array.push(b1);
-            } else {
-                array.push(b1);
-                array.push(a1);
-            }
-        }
+    let secondaryOrder;
+    if(filename1.toLowerCase() < filename2.toLowerCase()){
+        secondaryOrder = true;
+    } else{
+        secondaryOrder = false;
+    }
+    if(primaryOrder !== secondaryOrder){
+        return true;
+    } else{
+        return false;
     }
 }
